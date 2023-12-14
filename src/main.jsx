4c440bb773a388d216f38134a0ff3745410afafd
Mc508 <95183206+Mc508@users.noreply.github.com>
2023-12-14 11:31:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Component1 from './Component/Component1'
-import Editor from './Component/Editor'
-import Resume from './Component/Resume'
 import './index.css'
-import Personal from './Component/personal'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
